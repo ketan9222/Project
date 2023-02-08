900f6ad97cdbd51dfb4be48ea9082e8a51f815e1
ketan9222 <107883810+ketan9222@users.noreply.github.com>
2023-02-08 10:43:43 +0530

    region updated

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "ketan32-bucket"
     key = "main"
-    region = "us-east-2"
+    region = "us-east-1"
     dynamodb_table = "dynamodb-state-locking"
   }
 }
